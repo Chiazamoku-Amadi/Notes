6445d361caec286e0db6f42a957cb0514d72f2b8
Chiazamoku Amadi <91305429+Chiazamoku-Amadi@users.noreply.github.com>
2023-10-09 13:44:03 +0100

    refactor: Created new component for landing page and moved code there.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
+import Landing from "./Components/Landing/Landing";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center gap-6 h-screen">
-        <h1 className="text-5xl font-semibold">You have no notes</h1>
-        <button className="bg-app-color text-white text-xl py-2 px-8 rounded-lg font-medium">
-          Create Note
-        </button>
-      </div>
+      <Landing />
     </>
   );
 }
