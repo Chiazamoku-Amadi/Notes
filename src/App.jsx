1e2234ed7e9b84c6943288e08b9d1566f75a8a3f
Chiazamoku Amadi <91305429+Chiazamoku-Amadi@users.noreply.github.com>
2023-10-10 01:51:26 +0100

    build: Built the header of the all notes page. style: Added custom styles for the search bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
-import Landing from "./Components/Landing/Landing";
+import AllNotes from "./Components/AllNotes/AllNotes";
+// import Landing from "./Components/Landing/Landing";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(fas);
 
 function App() {
   return (
     <>
-      <Landing />
+      {/* <Landing /> */}
+      <AllNotes />
     </>
   );
 }
